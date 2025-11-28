e3c6a3b8597c2ca18fc370e08ba394a1fabf44fb
Renato Prado <renato.prado@fevo.com>
2025-11-28 10:19:23 -0300

    Adding cloudwatch agent

--- terraform/study/001_test_EC2/main.tf
+++ terraform/study/001_test_EC2/main.tf
@@ -1,3 +1,11 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_aws_cert_test"
+  description = "Security group for EC2 AWS Certification test instance"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+  #ingress {}
+  #egress {}
+}
+
 module "ec2_instance" {
   source = "../../modules/ec2"
   instance_type = "t2.micro"
@@ -5,7 +13,13 @@ module "ec2_instance" {
   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_ids["private-test"]
   project = "aws cert study"
   name = "test-ec2-instance"
+  security_group_ids = [aws_security_group.ec2_sg.id]
   tags = {
     Environment = "Test"
   }
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y amazon-cloudwatch-agent
+  EOF
 }
